6abb83a59ffc33ba329ca28c1a9ac6796cafef86
Martin Kearn <martin.kearn@microsoft.com>
2020-07-08 11:59:12 +0100

    Rename projects

--- src/Program.cs
+++ src/Program.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
-namespace BlazorServerMinimal
+namespace BlazorServerMinimalTemplate
 {
     public class Program
     {
--- src/Startup.cs
+++ src/Startup.cs
@@ -4,7 +4,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace BlazorServerMinimal
+namespace BlazorServerMinimalTemplate
 {
     public class Startup
     {
