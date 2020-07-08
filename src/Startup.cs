f15abfea22cd5709ec294ad22350759befe03edc
Martin Kearn <martin.kearn@microsoft.com>
2020-07-08 11:49:49 +0100

    Removed comments and default text

--- src/Startup.cs
+++ src/Startup.cs
@@ -15,15 +15,12 @@ namespace BlazorServerMinimal
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -33,7 +30,6 @@ namespace BlazorServerMinimal
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
